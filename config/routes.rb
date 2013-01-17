b16ef31b65b98c01e0eeadbbf30a0df4c784f4d3
Rodrigo Gouvea <rodrigosggouvea@gmail.com>
2013-01-17 18:07:08 -0200

    index created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 GameStoreUltra::Application.routes.draw do
+  root :to => 'application#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +49,6 @@ GameStoreUltra::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
