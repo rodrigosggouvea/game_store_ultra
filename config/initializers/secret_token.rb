995cc20a20716ad0a89ab7705ddc60be0727074f
Rodrigo Gouvea <rodrigosggouvea@gmail.com>
2013-01-17 17:31:53 -0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GameStoreUltra::Application.config.secret_token = 'b962b7c464b4929b77e56a7b982ca4da0960e351f3b6b59124ae90a0a91ffc06284dbfee172095deb8d29607c72b49bb614076b3632f8fed198ae135601d79a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GameStoreUltra::Application.config.session_store :cookie_store, key: '_game_store_ultra_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GameStoreUltra::Application.config.session_store :active_record_store
