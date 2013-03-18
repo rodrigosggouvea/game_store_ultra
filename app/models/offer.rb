d2b447110254e8df28af5299fb08f16c3e12b5d2
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2013-03-18 08:26:22 -0300

    correcao de erro no modelo offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,6 +2,8 @@ class Offer < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :platform_title
 	has_many :offer_images
+
+	
   attr_accessible :platform_title_id, :price, :text, :user_id, :title, :offer_images_attributes, :image
   accepts_nested_attributes_for :offer_images
 end
