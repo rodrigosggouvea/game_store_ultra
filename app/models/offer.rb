da7a343797c73cc63b3e4b4dee043e8bd340faf8
Rodrigo Gouvêa <rodrigosggouvea@gmail.com>
2013-03-21 11:06:17 -0300

    perguntas de ofertas

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,7 +3,7 @@ class Offer < ActiveRecord::Base
 	belongs_to :platform_title
 	has_many :offer_images
 	has_many :questions
-	attr_accessible :title, :price, :text, :offer_images_attributes
 	attr_accessor :title, :price, :text, :offer_images_attributes
+	attr_accessible :title, :price, :text, :offer_images_attributes
   accepts_nested_attributes_for :offer_images
 end
--- app/models/offer_image.rb
+++ app/models/offer_image.rb
@@ -1,4 +1,5 @@
 class OfferImage < ActiveRecord::Base
+	belongs_to :offer
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   attr_accessible :image, :_destroy
   attr_accessor :image, :_destroy
